ae2f540de8f6c15c580ff044e5f98e0ee7c70413
cokeefe <chaemobile@gmail.com>
2013-09-08 15:46:37 -0700

    add displayed to p tag

--- public/js/applicationSurvey.js
+++ public/js/applicationSurvey.js
@@ -10,32 +10,52 @@ Survey.prototype = {
     var input = '<input id="' + id + '"' + ' name="' + id + '" type="text">'
     $('#master_survey').prepend(input)
   },
-  showInputEvent: function(eventClick, displayTag){
+  showTitle: function(eventClick, displayTag){
     var that = this
 
     eventClick.on('click', function(e){
+        $('#add_choice').show();
       e.preventDefault();
       //save to form
-      var header = $('input:first').val();
-      console.log(header)
-      console.log(displayTag)
-      displayTag.text(header);
+      var userInput = $('input:first').val();
+      displayTag.text(userInput);
       //hide
       $('#master_survey input').hide();
       $(this).hide();
       //show
       $('#prompt').text('Create Question: ')
       that.questionSelector = 'question[' + that.questionCounter + ']'
-      console.log(that.questionSelector)
-      var selectinput = that.questionSelector
-      that.createInput(selectinput);
-      $('#add_choice').show();
+      that.createInput(that.questionSelector);
     })
+  },
+  showQuestion: function(displayTag){
+    var that = this
+
+    $('#form_buttons').on('click', '#add_choice', function(e){
+      e.preventDefault();
+      //increase counter
+      that.questionCounter += 1
+      //save to form
+      var userInput = $('input:first').val();
+      $('#displayed').append('<p>Question ' + that.questionCounter + ' : ' + userInput + '</p>');
+      //hide
+      $('#master_survey input').hide();
+      //show
+      $('#prompt').text('Create Answer Choice:');
+      that.choiceSelector = 'choice[' + that.choiceCounter + ']'
+      that.createInput(that.choiceSelector);
+      $('#add_question').show();
+      $('#confirm_survey').show();
+    })
+  },
+  showChoice: function(){
+
   }
 }
 
 //document ready
 $(function() {
   var survey = new Survey();
-  survey.showInputEvent($('#save'), $('#_formConfirmation h1'))
+  survey.showTitle($('#save'), $('#_formConfirmation h1'))
+  survey.showQuestion($('#prompt'))
 })
