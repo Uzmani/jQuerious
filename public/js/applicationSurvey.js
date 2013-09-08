ef1039e10b8fc876efcff5441136fe45761ddab2
cokeefe <chaemobile@gmail.com>
2013-09-08 16:34:51 -0700

    input fields dynamic, names match migrations

--- public/js/applicationSurvey.js
+++ public/js/applicationSurvey.js
@@ -10,6 +10,10 @@ Survey.prototype = {
     var input = '<input id="' + id + '"' + ' name="' + id + '" type="text">'
     $('#master_survey').prepend(input)
   },
+  createHiddenInput: function(id){
+    var input = '<input id="' + id + '"' + ' value="' + this.questionCounter + '"'+ ' name="' + id + '" type="hidden">'
+    $('#master_survey').prepend(input)
+  },
   showTitle: function(eventClick, displayTag){
     var that = this
 
@@ -23,7 +27,8 @@ Survey.prototype = {
       $(this).hide();
       //show
       $('#prompt').text('Create Question: ')
-      that.questionSelector = 'question[' + that.questionCounter + ']'
+      that.questionCounter += 1
+      that.questionSelector = 'question[' + that.questionCounter + '][title]'
       that.createInput(that.questionSelector);
       $('#save_question').show();
     })
@@ -32,7 +37,6 @@ Survey.prototype = {
     var that = this
     $('#form_buttons').on('click', '#save_question', function(e){
       e.preventDefault();
-      that.questionCounter += 1
       //save to form
       var userInput = $('input:first').val();
       $('#displayed').append('<p>Question ' + that.questionCounter + ' : ' + userInput + '</p>');
@@ -41,13 +45,32 @@ Survey.prototype = {
       $('#save_question').hide();
       //show
       $('#prompt').text('Create Answer Choice:');
-      that.choiceSelector = 'choice[' + that.choiceCounter + ']'
+      that.choiceCounter += 1
+      var hiddenSelector = 'choice[' + that.choiceCounter + '][question]'
+      that.createHiddenInput(hiddenSelector)
+      console.log('CHECK NEW CHOICE INPUT TAG FOR VALUE')
+      that.choiceSelector = 'choice[' + that.choiceCounter + '][option]'
       that.createInput(that.choiceSelector);
-      $('#')
+
+      $('#save_choice').show();
     })
   },
   showChoice: function(){
-
+    var that = this
+    $('#form_buttons').on('click', '#save_choice', function(e){
+      e.preventDefault();
+      //save to form
+      var userInput = $('input:first').val();
+      $('#displayed').append('<p>Choice ' + that.choiceCounter + ' : ' + userInput + '</p>');
+      //hide
+      $('#master_survey input').hide();
+      $('#prompt').hide()
+      $('#save_choice').hide();
+      //show
+      $('#add_question').show();
+      $('#add_choice').show();
+      $('#confirm_survey').show();
+    })
   }
 }
 
@@ -56,4 +79,5 @@ $(function() {
   var survey = new Survey();
   survey.showTitle($('#save'), $('#_formConfirmation h1'))
   survey.saveQuestion()
+  survey.showChoice()
 })
