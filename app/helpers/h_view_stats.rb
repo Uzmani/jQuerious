cd3f32503a16582525867de6bbd62812e1324302
Lily <qvang.j@gmail.com>
2013-09-07 18:54:58 -0700

    working on stat mechanism

---
+++ app/helpers/h_view_stats.rb
@@ -0,0 +1,15 @@
+helpers do
+  def stat_class
+
+  end
+
+end
+
+for each question's choices 
+count the number of all answers that has that choice's id
+
+choices = Choice.all
+
+choices.each do |choice|
+  
+end
